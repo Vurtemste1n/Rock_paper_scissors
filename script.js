a881aa8aac1f15144ccea02ac8ca171c2eefada6
Vurtemste1n <danil@armanshin.ru>
2023-11-02 05:18:05 +0300

    Write function for one round and for game

--- script.js
+++ script.js
@@ -10,5 +10,44 @@ function getComputerChoice() {
     } else if (randNum == 1) {
         choice = 'paper';
     } else choice = 'scissors';
+    return choice;
 }
-console.log(getComputerChoice());
+function round(playerSelection,computerSelection) {
+    let message = "";
+    if (computerSelection == 'rock' && playerSelection == 'rock') {
+        message = ('You are even!');
+    } else if (playerSelection == 'paper' && computerSelection == 'rock') {
+        message = (playerSelection + " beats " + computerSelection + ". You win!");
+    } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
+        message = (playerSelection + " beats " + computerSelection + ". You win!");
+    } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
+        message = (playerSelection + " beats " + computerSelection + ". You win!");
+    } else {
+        message = (computerSelection + " beats " + playerSelection + ". You lost!");
+    }
+    return message;
+}
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    for (let i = 0; i <= 4; i++) {
+        let computerSelection = getComputerChoice();
+        let playerSelection = prompt("Enter your choice here: ");
+        console.log(round(playerSelection,computerSelection));
+        if (round(playerSelection,computerSelection).substr(-8) == "You win!" ) {
+            playerScore = ++playerScore;
+        }
+        if (round(playerSelection,computerSelection).substr(-9) == "You lost!" ) {
+            computerScore = ++computerScore;
+        }
+    }
+    if (playerScore == computerScore) {
+        console.log("Yor are even! You and your computer have equal score.")
+    }
+    else if (playerScore > computerScore) {
+        console.log("You have won " + playerScore + " rounds. Your computer has won " + computerScore + " rounds.You won the game!");
+    } else console.log("You have won " + playerScore + " rounds. Your computer has won " + computerScore + " rounds.You lost the game!");
+    return ;
+}
+
+(game());
