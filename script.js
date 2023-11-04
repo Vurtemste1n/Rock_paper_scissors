0bb11bddb029cd15bf3fe7f77a22ff209c3bcc9d
Vurtemste1n <danil@armanshin.ru>
2023-11-04 03:38:56 +0300

    Add styling

--- script.js
+++ script.js
@@ -6,22 +6,23 @@ function getComputerChoice() {
     let choice = '';
     let randNum = Math.floor(Math.random()*(n+1));
     if (randNum == 0) {
-        choice = 'rock';
+        choice = '✊';
     } else if (randNum == 1) {
-        choice = 'paper';
-    } else choice = 'scissors';
+        choice = '✋';
+    } else choice = '✌';
     return choice;
 }
 function round(playerSelection,computerSelection) {
     let message = "";
     if (computerSelection == playerSelection) {
         message = ('You are even!');
-    } else if (playerSelection == "paper" && computerSelection == "rock") {
+    } else if (playerSelection == "✋" && computerSelection == "✊") {
         message = (firstLetter(playerSelection) + " beats " + computerSelection + ". You win!");
-    } else if (playerSelection == "rock" && computerSelection == "scissors") {
+    } else if (playerSelection == "✊" && computerSelection == "✌") {
         message = (firstLetter(playerSelection) + " beats " + computerSelection + ". You win!");
-    } else if (playerSelection == "scissors" && computerSelection == "paper") {
+    } else if (playerSelection == "✌" && computerSelection == "✋") {
         message = (firstLetter(playerSelection) + " beats " + computerSelection + ". You win!");
+
     } else 
         message = (firstLetter(computerSelection) + " beats " + playerSelection + ". You lost!");
     
@@ -55,14 +56,20 @@ function firstLetter(string) {
     const result = document.querySelector("#result");
     const playerScoreTracker = document.querySelector("#playerScore");
     const computerScoreTracker = document.querySelector("#computerScore");
+    const playerChoice = document.querySelector(".player-choice");
+    const computerChoice = document.querySelector(".computer-choice");
     playerScoreTracker.textContent = "0";
     computerScoreTracker.textContent = "0";
     let scoreTracker; 
+    const para = document.createElement('p');
+    result.appendChild(para);
     btnRock.addEventListener("click",() => {
+        computerChoice.textContent = getComputerChoice();
+        playerChoice.textContent = '✊';
         playerSelection = btnRock.textContent;
-        const para = document.createElement('p');
-        result.appendChild(para);
-        para.textContent = round(caseSens(playerSelection),getComputerChoice());
+        result.classList.remove('green-para');
+        result.classList.remove('red-para');
+        para.textContent = round(playerSelection,computerChoice.textContent);
         scoreTracker = score(para.textContent);
         playerScoreTracker.textContent = `${Number(playerScoreTracker.textContent) + scoreTracker[0]}`;
         computerScoreTracker.textContent = `${Number(computerScoreTracker.textContent) + scoreTracker[1]}`;
@@ -72,19 +79,24 @@ function firstLetter(string) {
                 computerScoreTracker.textContent = '0';
                 para.textContent = "Player has won!";
                 result.appendChild(para);
+                result.classList.add("green-para");
             } else if (computerScoreTracker.textContent == '5') {
                 result.textContent = '';
                 playerScoreTracker.textContent = '0';
                 computerScoreTracker.textContent = '0';
-                para.textContent = "Player has won!";
+                para.textContent = "Computer has won!";
                 result.appendChild(para);
+                result.classList.add("red-para");
+
             }
     });
     btnPaper.addEventListener("click",() => {
+        computerChoice.textContent = getComputerChoice();
+        playerChoice.textContent = '✋';
+        result.classList.remove('green-para');
+        result.classList.remove('red-para');
         playerSelection = btnPaper.textContent;
-        const para = document.createElement('p');
-        result.appendChild(para);
-        para.textContent = round(caseSens(playerSelection),getComputerChoice());
+        para.textContent = round(caseSens(playerSelection),computerChoice.textContent);
         scoreTracker = score(para.textContent);
         playerScoreTracker.textContent = `${Number(playerScoreTracker.textContent) + scoreTracker[0]}`;
         computerScoreTracker.textContent = `${Number(computerScoreTracker.textContent) + scoreTracker[1]}`;
@@ -94,20 +106,25 @@ function firstLetter(string) {
             computerScoreTracker.textContent = '0';
             para.textContent = "Player has won!";
             result.appendChild(para);
+            result.classList.add("green-para");
         } else if (computerScoreTracker.textContent == '5') {
             result.textContent = "";
             playerScoreTracker.textContent = '0';
             computerScoreTracker.textContent = '0';
             para.textContent = "Computer has won!";
             result.appendChild(para);
+            result.classList.add("red-para");
+
         }
 
     });
         btnScissors.addEventListener("click",() => {
+        computerChoice.textContent = getComputerChoice();
+        playerChoice.textContent = '✌';
+        result.classList.remove('green-para');
+        result.classList.remove('red-para');    
         playerSelection = btnScissors.textContent;
-        const para = document.createElement('p');
-        result.appendChild(para);
-        para.textContent = round(caseSens(playerSelection),getComputerChoice());
+        para.textContent = round(playerChoice.textContent,computerChoice.textContent);
         scoreTracker = score(para.textContent);
         playerScoreTracker.textContent = `${Number(playerScoreTracker.textContent) + scoreTracker[0]}`;
         computerScoreTracker.textContent = `${Number(computerScoreTracker.textContent) + scoreTracker[1]}`;
@@ -117,20 +134,24 @@ function firstLetter(string) {
             computerScoreTracker.textContent = '0';
             para.textContent = "Player has won!";
             result.appendChild(para);
+            result.classList.add("green-para");
         } else if (computerScoreTracker.textContent == '5') {
             result.textContent = '';
             playerScoreTracker.textContent = '0';
             computerScoreTracker.textContent = '0';
-            para.textContent = "Player has won!";
+            para.textContent = "Computer has won!";
             result.appendChild(para);
+            result.classList.add("red-para");
         }
 
     }); 
 
 btnClear.addEventListener("click",() => {
-    result.textContent = '';
+    para.textContent = '';
     playerScoreTracker.textContent = '0';
     computerScoreTracker.textContent = '0';
+    playerChoice.textContent = "";
+    computerChoice.textContent = "";
 });
 
 
